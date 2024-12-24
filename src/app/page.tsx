bf71868d6345cf40da07c90be1e69f61a2f1eb7e
mehedihasan712277 <mehedihasan712277@gmail.com>
2024-12-24 19:03:59 +0600

    style added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <h1>this is h1</h1>
+      <h2>this is h2</h2>
+      <ul>
+        <li>this is ul li 1</li>
+        <li>this is ul li 2</li>
+      </ul>
+    </div>
   )
 }
 
